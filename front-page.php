5527437daa47f49f4ac1695bdfa5767529fec6cb
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-10-31 20:17:06 +0100

    added file structure and basic layout

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+    get_header();
+?>
+
+<header id="main-header">
+
+</header>
+<main id="content-area">
+
+</main>
+<footer id="main-footer">
+
+</footer>
+
+<?php
+    get_footer();
+?>
+
---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function fp_theme_support(){
+    add_theme_support( 'title-tag');
+}
+
+add_action('after_setup_theme', 'fp_theme_support');
+
+function fp_theme_styles(){
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('fp_theme_style', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+}
+
+add_action('wp_enqueue_scripts', 'fp_theme_styles');
+
+?>
