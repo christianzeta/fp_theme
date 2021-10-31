d424380b750b3da72aeaedcd8bca489fec7590e1
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-10-31 21:21:56 +0100

    added a widget in the footer

--- front-page.php
+++ front-page.php
@@ -16,7 +16,9 @@
     </section>
 </main>
 <footer id="main-footer">
-
+    <div id="main-footer-widget">
+        <?php dynamic_sidebar('footer') ?> 
+    </div>
 </footer>
 
 <?php
--- functions.php
+++ functions.php
@@ -13,4 +13,20 @@ function fp_theme_styles(){
 
 add_action('wp_enqueue_scripts', 'fp_theme_styles');
 
+function fp_theme_widget_areas(){
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '',
+            'after_widget' => '',
+            'name' => 'Frontpage Footer Area',
+            'id' => 'footer',
+            'description' => 'Frontpage Footer Area'
+        )     
+    );
+}
+
+add_action('widgets_init','fp_theme_widget_areas');
+
 ?>
