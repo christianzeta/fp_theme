3655aabbb1c7b7b479133f97cf338e45250baed3
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-10-31 21:05:19 +0100

    added text and image with advanced custom fields

--- front-page.php
+++ front-page.php
@@ -6,7 +6,14 @@
     <h1><?php the_field('change_page_title') ?></h1>
 </header>
 <main id="content-area">
-
+    <section id="main-content">
+        <div id="main-content-image">
+            <img src="<?php the_field('main_image'); ?>" width="100%" height="100%" />
+        </div>
+        <div id="main-content-text">
+            <p><?php the_field('main_text') ?></p>
+        </div>
+    </section>
 </main>
 <footer id="main-footer">
 
